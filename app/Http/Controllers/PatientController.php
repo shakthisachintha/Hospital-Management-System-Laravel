7d87129cfe2744d8060465d843520c0067f84b80
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2019-08-29 19:55:10 +0530

    patiet registration ok

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App;
+use App\Patients;
+
 class PatientController extends Controller
 {
     public function __construct()
@@ -22,10 +23,14 @@ class PatientController extends Controller
     public function register_patient(Request $request)
     {
         $user = Auth::user();
-        $this->validate($request,[
-            'name' => 'required'
-        ]);
-        return 123;
-        // return view('patient.register_patient',['title'=>$user->name]);
+        //dd($request->all());
+        $patient=new Patients;
+        $patient->name=$request->reg_pname;
+        $patient->address=$request->reg_paddress;
+        $patient->occupation=$request->reg_poccupation;
+        $patient->sex=$request->reg_psex;
+        $patient->age=$request->reg_page;
+        $patient->save();
+        return redirect()->back();
     }
 }
