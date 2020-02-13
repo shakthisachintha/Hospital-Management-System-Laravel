05a03148ad44a674f3c782e41cba24c104084174
shakthisachintha <43114582+shakthisachintha@users.noreply.github.com>
2020-02-13 21:11:50 +0530

    clinics migrations

---
+++ database/migrations/2020_02_13_201718_create_clinics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClinicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_eng');
+            $table->string('name_sin');
+            $table->string('recuring');
+            $table->date('start-date');
+            $table->bigInteger('doctor_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clinics');
+    }
+}
