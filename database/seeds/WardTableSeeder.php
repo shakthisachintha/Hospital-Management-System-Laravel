29488fd35e605d6979f37e0c41b3f63c68a231a9
sachindesilva <sachintharindu95@gmail.com>
2020-02-19 11:58:38 +0530

    ward table seeder

--- database/seeds/WardTableSeeder.php
+++ database/seeds/WardTableSeeder.php
@@ -17,7 +17,7 @@ class WardTableSeeder extends Seeder
                 'ward_no' => '03',
                 'beds' => '10',
                 'free_beds' => '5',
-                'doctor_id' => '523311',
+                'doctor_id' => '1',
                 'created_at' => '2019-11-27 12:41:40',
                 'updated_at' => '2019-11-27 12:41:41',
             ], 
@@ -26,7 +26,7 @@ class WardTableSeeder extends Seeder
                 'ward_no' => '06',
                 'beds' => '6',
                 'free_beds' => '3',
-                'doctor_id' => '522310',
+                'doctor_id' => '2',
                 'created_at' => '2019-11-27 12:41:40',
                 'updated_at' => '2019-11-27 12:41:41',
             ]
